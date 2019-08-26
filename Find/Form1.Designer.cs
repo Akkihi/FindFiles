ce297ff32918d1378d7459bcbeb588a82fee4d9f
Akkihi <akkihiboy@gmail.com>
2019-08-26 07:47:19 +0300

    Добавьте файлы проекта.

---
+++ Find/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Find
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Path_Text = new System.Windows.Forms.TextBox();
+            this.Filename_Text = new System.Windows.Forms.TextBox();
+            this.Path_Btn = new System.Windows.Forms.Button();
+            this.Filename_Btn = new System.Windows.Forms.Button();
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.WordinFile_Text = new System.Windows.Forms.TextBox();
+            this.ListView = new System.Windows.Forms.ListView();
+            this.Filename_Label = new System.Windows.Forms.Label();
+            this.StopSearch_Btn = new System.Windows.Forms.Button();
+            this.ResumeSearch_Btn = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.Timer_Label = new System.Windows.Forms.Label();
+            this.Akkihiinfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Path_Text
+            // 
+            this.Path_Text.Location = new System.Drawing.Point(21, 12);
+            this.Path_Text.Name = "Path_Text";
+            this.Path_Text.Size = new System.Drawing.Size(417, 20);
+            this.Path_Text.TabIndex = 0;
+            this.Path_Text.TextChanged += new System.EventHandler(this.Path_Text_TextChanged);
+            // 
+            // Filename_Text
+            // 
+            this.Filename_Text.Location = new System.Drawing.Point(21, 47);
+            this.Filename_Text.Name = "Filename_Text";
+            this.Filename_Text.Size = new System.Drawing.Size(417, 20);
+            this.Filename_Text.TabIndex = 1;
+            // 
+            // Path_Btn
+            // 
+            this.Path_Btn.Location = new System.Drawing.Point(444, 12);
+            this.Path_Btn.Name = "Path_Btn";
+            this.Path_Btn.Size = new System.Drawing.Size(28, 20);
+            this.Path_Btn.TabIndex = 2;
+            this.Path_Btn.Text = "...";
+            this.Path_Btn.UseVisualStyleBackColor = true;
+            this.Path_Btn.Click += new System.EventHandler(this.Path_Btn_Click);
+            // 
+            // Filename_Btn
+            // 
+            this.Filename_Btn.Enabled = false;
+            this.Filename_Btn.Location = new System.Drawing.Point(445, 201);
+            this.Filename_Btn.Name = "Filename_Btn";
+            this.Filename_Btn.Size = new System.Drawing.Size(127, 20);
+            this.Filename_Btn.TabIndex = 3;
+            this.Filename_Btn.Text = "Новый Поиск";
+            this.Filename_Btn.UseVisualStyleBackColor = true;
+            this.Filename_Btn.Click += new System.EventHandler(this.Filename_Btn_Click);
+            // 
+            // WordinFile_Text
+            // 
+            this.WordinFile_Text.Location = new System.Drawing.Point(21, 78);
+            this.WordinFile_Text.Name = "WordinFile_Text";
+            this.WordinFile_Text.Size = new System.Drawing.Size(417, 20);
+            this.WordinFile_Text.TabIndex = 6;
+            this.WordinFile_Text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ListView
+            // 
+            this.ListView.FullRowSelect = true;
+            this.ListView.GridLines = true;
+            this.ListView.HideSelection = false;
+            this.ListView.Location = new System.Drawing.Point(21, 115);
+            this.ListView.Name = "ListView";
+            this.ListView.Size = new System.Drawing.Size(417, 302);
+            this.ListView.TabIndex = 7;
+            this.ListView.UseCompatibleStateImageBehavior = false;
+            this.ListView.View = System.Windows.Forms.View.List;
+            this.ListView.SelectedIndexChanged += new System.EventHandler(this.ListView_SelectedIndexChanged_1);
+            this.ListView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListView_MouseDoubleClick);
+            // 
+            // Filename_Label
+            // 
+            this.Filename_Label.AutoSize = true;
+            this.Filename_Label.Location = new System.Drawing.Point(18, 430);
+            this.Filename_Label.Name = "Filename_Label";
+            this.Filename_Label.Size = new System.Drawing.Size(0, 13);
+            this.Filename_Label.TabIndex = 8;
+            // 
+            // StopSearch_Btn
+            // 
+            this.StopSearch_Btn.Enabled = false;
+            this.StopSearch_Btn.Location = new System.Drawing.Point(445, 237);
+            this.StopSearch_Btn.Name = "StopSearch_Btn";
+            this.StopSearch_Btn.Size = new System.Drawing.Size(127, 20);
+            this.StopSearch_Btn.TabIndex = 9;
+            this.StopSearch_Btn.Text = "Остановить поиск";
+            this.StopSearch_Btn.UseVisualStyleBackColor = true;
+            this.StopSearch_Btn.Click += new System.EventHandler(this.StopSearch_Btn_Click);
+            // 
+            // ResumeSearch_Btn
+            // 
+            this.ResumeSearch_Btn.Enabled = false;
+            this.ResumeSearch_Btn.Location = new System.Drawing.Point(445, 273);
+            this.ResumeSearch_Btn.Name = "ResumeSearch_Btn";
+            this.ResumeSearch_Btn.Size = new System.Drawing.Size(127, 20);
+            this.ResumeSearch_Btn.TabIndex = 10;
+            this.ResumeSearch_Btn.Text = "Продолжить поиск";
+            this.ResumeSearch_Btn.UseVisualStyleBackColor = true;
+            this.ResumeSearch_Btn.Click += new System.EventHandler(this.ResumeSearch_Btn_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.PrivateTimer);
+            // 
+            // Timer_Label
+            // 
+            this.Timer_Label.AutoSize = true;
+            this.Timer_Label.Location = new System.Drawing.Point(18, 467);
+            this.Timer_Label.Name = "Timer_Label";
+            this.Timer_Label.Size = new System.Drawing.Size(0, 13);
+            this.Timer_Label.TabIndex = 11;
+            // 
+            // Akkihiinfo
+            // 
+            this.Akkihiinfo.AutoSize = true;
+            this.Akkihiinfo.Location = new System.Drawing.Point(1063, 194);
+            this.Akkihiinfo.Name = "Akkihiinfo";
+            this.Akkihiinfo.Size = new System.Drawing.Size(56, 13);
+            this.Akkihiinfo.TabIndex = 12;
+            this.Akkihiinfo.Text = "Akkihi.info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 491);
+            this.Controls.Add(this.Akkihiinfo);
+            this.Controls.Add(this.Timer_Label);
+            this.Controls.Add(this.ResumeSearch_Btn);
+            this.Controls.Add(this.StopSearch_Btn);
+            this.Controls.Add(this.Filename_Label);
+            this.Controls.Add(this.ListView);
+            this.Controls.Add(this.WordinFile_Text);
+            this.Controls.Add(this.Filename_Btn);
+            this.Controls.Add(this.Path_Btn);
+            this.Controls.Add(this.Filename_Text);
+            this.Controls.Add(this.Path_Text);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 530);
+            this.MinimumSize = new System.Drawing.Size(800, 530);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поиск файлов по критериям";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Path_Text;
+        private System.Windows.Forms.TextBox Filename_Text;
+        private System.Windows.Forms.Button Path_Btn;
+        private System.Windows.Forms.Button Filename_Btn;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+        private System.Windows.Forms.TextBox WordinFile_Text;
+        private System.Windows.Forms.Label Filename_Label;
+        public System.Windows.Forms.ListView ListView;
+        private System.Windows.Forms.Button StopSearch_Btn;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Label Timer_Label;
+        public System.Windows.Forms.Button ResumeSearch_Btn;
+        private System.Windows.Forms.Label Akkihiinfo;
+    }
+}
+
---
+++ Find/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Find
+{
+    public partial class Form1 : Form
+    {
+        public static string pathfolders = "";
+
+        public static string filenames;
+
+        public static string wordfiles;
+
+        public static string wordtitle;
+
+        public static int countfiles;
+
+        public static int lastcountfiles;
+
+        public static List<string> ListV = new List<string>();
+
+        Queue<string> qe = new Queue<string>();
+
+        public static string pauseresume;
+
+        TimeSpan time1;
+
+        DateTime initial_time = DateTime.Now;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pauseresume = "pause";
+            wordfiles = "";
+            Timer.Stop();
+        }
+
+
+        void TrueFalse()
+        {
+            if (Path_Text.Text == null && pauseresume == "pause")
+            {
+                Filename_Btn.Enabled = false;
+                StopSearch_Btn.Enabled = false;
+                ResumeSearch_Btn.Enabled = false;
+            }
+            else
+            {
+                Filename_Btn.Enabled = true;
+                StopSearch_Btn.Enabled = true;
+                ResumeSearch_Btn.Enabled = true;
+            }
+            if (pathfolders == null && pauseresume == "pause")
+            {
+                Filename_Btn.Enabled = false;
+                StopSearch_Btn.Enabled = false;
+                ResumeSearch_Btn.Enabled = false;
+            }
+            else
+            {
+                Filename_Btn.Enabled = true;
+                StopSearch_Btn.Enabled = true;
+                ResumeSearch_Btn.Enabled = true;
+            }
+
+            if (pauseresume == "resume")
+            {
+                Filename_Btn.Enabled = false;
+            }
+            else
+            {
+                Filename_Btn.Enabled = true;
+
+            }
+
+            if (pauseresume == "pause")
+            {
+                StopSearch_Btn.Enabled = false;
+            }
+            else
+            {
+                StopSearch_Btn.Enabled = true;
+
+            }
+            if (pauseresume == "pause")
+            {
+                ResumeSearch_Btn.Enabled = true;
+            }
+            else
+            {
+                ResumeSearch_Btn.Enabled = false;
+
+            }
+
+        }
+
+
+        private void Path_Btn_Click(object sender, EventArgs e)
+        {
+            TrueFalse();
+            pathfolders = Path_Text.Text;
+            Path_Text.Text = FBD.SelectedPath;
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                Path_Text.Text = FBD.SelectedPath;
+                pathfolders = Path_Text.Text;
+
+            }
+
+
+        }
+
+        public void Filename_Btn_Click(object sender, EventArgs e)
+        {
+
+            wordfiles = WordinFile_Text.Text;
+            lastcountfiles = 0;
+            qe.Clear();
+            ListView.Items.Clear();
+            filenames = Filename_Text.Text;
+            if (filenames == "")
+            {
+                filenames = "*";
+            }
+            Searching();
+            pauseresume = "resume";
+            TrueFalse();
+            Timer.Stop();
+            Timer.Dispose();
+            PrivateTimer();
+        }
+
+        private void StopSearch_Btn_Click(object sender, EventArgs e)
+        {
+            Timer.Enabled = false;
+            Timer.Stop();
+            pauseresume = "pause";
+            lastcountfiles = countfiles;
+            PrivateTimer();
+            TrueFalse();
+
+        }
+
+        private void ResumeSearch_Btn_Click(object sender, EventArgs e)
+        {
+
+            filenames = Filename_Text.Text;
+            if (filenames == "")
+            {
+                filenames = "*";
+            }
+            pauseresume = "resume";
+            PrivateTimer();
+            TrueFalse();
+            Searching();
+        }
+
+        public async void Searching()
+        {
+            await Task.Run(() =>
+            {
+
+                DirectoryInfo Dir = new DirectoryInfo(pathfolders);
+
+                var files = Dir.GetFiles(filenames, SearchOption.AllDirectories);
+
+
+                if (pauseresume == "resume")
+                {
+
+                    for (countfiles = lastcountfiles; countfiles < files.Length; countfiles++)
+                    {
+ 
+                        if (wordfiles != "")
+                        {
+                            var stream = new StreamReader(files[countfiles].FullName);
+                            while (!stream.EndOfStream)
+                            {
+                                var line = stream.ReadLine();
+                                foreach (Match a in Regex.Matches(line, wordfiles, RegexOptions.IgnoreCase))
+                                {
+                                    wordtitle = files[countfiles].FullName;
+
+                                }
+
+                            }
+                            stream.Dispose();
+
+                            if (!ListV.Contains(wordtitle))
+                            {
+                                ListV.Add(wordtitle);
+                                qe.Enqueue(wordtitle);
+                            }
+                        }
+                        else
+                        {
+                            ListV.Add(files[countfiles].FullName);
+                            qe.Enqueue(files[countfiles].Name);
+                        }
+
+                        AddList();
+                        Action action = () =>
+                        {
+                            if (files.Length == ListView.Items.Count)
+                            {
+                                pauseresume = "pause";
+                                TrueFalse();
+                                Timer.Stop();
+                                StopSearch_Btn.Enabled = false;
+                                ResumeSearch_Btn.Enabled = false;
+                                Timer_Label.Text = "Структура загружена за: " + time1.Minutes.ToString() + " минут : " + time1.Seconds.ToString() + " секунд : " + time1.Milliseconds.ToString() + " миллисекунд.";
+                            }
+                        };
+                        if (InvokeRequired)
+                            Invoke(action);
+                        else
+                            action();
+
+                    }
+                }
+
+            });
+        }
+
+        public void AddList()
+        {
+            if (pauseresume == "resume")
+            {
+                Action action = () =>
+                {
+                    if (qe.Count > 0)
+                    {
+                        Filename_Label.Text = "Кол-во файлов просчитано: " + ListView.Items.Count + "\n" + "Последний обработанный файл:" + ListView.Items.Add(qe.Dequeue());
+                        Timer_Label.Text = "Загрузка структуры: " + time1.Minutes.ToString() + " минут : " + time1.Seconds.ToString() + " секунд : " + time1.Milliseconds.ToString() + " миллисекунд.";
+                        Timer_Label.Refresh();
+                        PrivateTimer();
+                    }
+                };
+                if (InvokeRequired)
+                    Invoke(action);
+                else
+                    action();
+                Timer.Start();
+            }
+
+        }
+
+        private void ListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Filename_Label_Update(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Filename_Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void PrivateTimer()
+        {
+                DateTime current_time = DateTime.Now;
+                time1 = current_time - initial_time;
+                Timer.Enabled = true;            
+        }
+        private void PrivateTimer(object sender, EventArgs e)
+        {
+
+            
+        }
+
+
+
+        private void Path_Text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (ListView.SelectedIndices.Count == 1)
+            {
+                Process.Start(ListV[ListView.SelectedIndices[0]]);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
